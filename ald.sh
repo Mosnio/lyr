72bf3fc99de2d13ef5c9b55ff19c2d5cd842db73
Mosnio <130111318+Mosnio@users.noreply.github.com>
2023-07-08 20:07:25 +0530

    Update ald.sh

--- ald.sh
+++ ald.sh
@@ -1,27 +1,27 @@
- apt update
- apt install wget -y 
- apt-get install libdbus-glib-1-2 -y 
- apt-get install -y libgtk-3-0
- apt-get install sudo dialog -y 
- apt-get -y install apt-utils unzip 
- echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections 
- apt-get install -y -q
- apt install python3-pip -y
- pip3 install selenium 
- pip3 install tbselenium 
- apt install tor -y > /dev/null
+ sudo apt update
+ sudo apt install wget -y 
+ sudo apt-get install libdbus-glib-1-2 -y 
+ sudo apt-get install -y libgtk-3-0
+ sudo apt-get install sudo dialog -y 
+ sudo apt-get -y install apt-utils unzip 
+ sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections 
+ sudo apt-get install -y -q
+ sudo apt install python3-pip -y
+ sudo pip3 install selenium 
+ sudo pip3 install tbselenium 
+ sudo apt install tor -y > /dev/null
  sleep 25
- service tor start 
+ sudo service tor start 
  cd /home
  wget https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz 
- tar -xvf geckodriver-v0.31.0-linux64.tar.gz 
- chmod +x geckodriver 
+ sudo tar -xvf geckodriver-v0.31.0-linux64.tar.gz 
+ sudo chmod +x geckodriver 
  sudo cp geckodriver /usr/local/bin/ 
  geckodriver --version 
  wget https://www.torproject.org/dist/torbrowser/12.5.1/tor-browser-linux64-12.5.1_ALL.tar.xz
- tar -xvJf tor-browser-linux64-12.5.1_ALL.tar.xz
- apt-get install xvfb -y 
+ sudo tar -xvJf tor-browser-linux64-12.5.1_ALL.tar.xz
+ sudo apt-get install xvfb -y 
  cd /home
  wget https://github.com/Mosnio/yamigv/raw/main/rn.py
- 
+ sudo export MOZ_HEADLESS=1
  python3 rn.py
