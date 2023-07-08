fd4238a31284435a937d43c73ed4191645f9854e
Mosnio <130111318+Mosnio@users.noreply.github.com>
2023-07-08 22:15:11 +0530

    Update ald.sh

--- ald.sh
+++ ald.sh
@@ -18,10 +18,10 @@
  sudo chmod +x geckodriver 
  sudo cp geckodriver /usr/local/bin/ 
  geckodriver --version 
- wget https://www.torproject.org/dist/torbrowser/12.5.1/tor-browser-linux64-12.5.1_ALL.tar.xz
- sudo tar -xvJf tor-browser-linux64-12.5.1_ALL.tar.xz
- sudo apt-get install xvfb -y 
- cd /home
+ wget -O tbr.tar.xz https://www.torproject.org/dist/torbrowser/12.5.1/tor-browser-linux64-12.5.1_ALL.tar.xz
+ sudo tar -xvJf tbr.tar.xz
+ sudo apt-get install xvfb -y
  wget https://github.com/Mosnio/yamigv/raw/main/rn.py
  export MOZ_HEADLESS=1
+ sudo chmod +x rn.py
  python3 rn.py
